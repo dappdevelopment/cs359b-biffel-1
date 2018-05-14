15589965356519a2dbaa13e41699376ce38d3ee5
jlee29 <jlee29@stanford.edu>
2018-05-14 03:22:40 -0700

    testing web3 again

--- biffel/react-app/src/api.js
+++ biffel/react-app/src/api.js
@@ -3,7 +3,11 @@
 // solution would be more appropriate.
 console.log("hello world");
 var Web3 = require ('web3');
-if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
+if (typeof web3 == 'undefined') {
+  throw 'No web3 detected. Is Metamask/Mist being used?';
+} else {
+  console.log("we good");
+}
 // web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
 // console.log("Using web3 version: " + Web3.version);
 
