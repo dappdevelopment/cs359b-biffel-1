4801d8ffd8ed43258483c69f46041ecde9a1fdd1
jlee29 <jlee29@stanford.edu>
2018-05-14 02:54:08 -0700

    testing web3 again

--- biffel/react-app/src/api.js
+++ biffel/react-app/src/api.js
@@ -2,7 +2,7 @@
 // components. On a real website, a more robust data fetching
 // solution would be more appropriate.
 console.log("hello world");
-var Web3 = require("web3");
+// var Web3 = require("web3");
 if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
 // web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
 // console.log("Using web3 version: " + Web3.version);
