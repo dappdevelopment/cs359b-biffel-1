f60bac64d3e2490b280ac88782ae577135e75f1c
Jaime Deverall <jaime1deverall@gmail.com>
2018-06-03 22:24:28 -0700

    added buySlot

--- biffel/react-app/src/actions/buySlot.js
+++ biffel/react-app/src/actions/buySlot.js
@@ -1,8 +1,8 @@
 import {BUY_SLOT_SUCCESS, BUY_SLOT_FAILED} from './actionTypes';
 
-export default function buySlot(values, web3) {
+export default function buySlot(web3, biffelID) {
   return dispatch => {
-    web3.contract.methods.buySlot(values.biffelID).send({from: web3.userAccount})
+    web3.contract.methods.buySlot(biffelID).send({from: web3.userAccount})
     .then((res) => {
       console.log('res', res);
       dispatch({type: BUY_SLOT_SUCCESS});
--- biffel/react-app/src/components/DetailedItem.js
+++ biffel/react-app/src/components/DetailedItem.js
@@ -1,50 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import fetchItemDetails from '../actions/fetchItemDetails'
+import buySlot from '../actions/buySlot'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
+import { Button, FormGroup, FormControl, ControlLabel, Alert, Panel, Radio } from "react-bootstrap";
 
 // The Player looks up the player using the number parsed from
 // the URL's pathname. If no player is found with the given
 // number, then a "player not found" message is displayed.
-const DetailedItem = (props) => {
-  if(props.items === null){
-    return null
+
+class DetailedItem extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  let id = parseInt(props.match.params.id);
-
-  if (isNaN(id)) {
-    return <div>Sorry, but no item</div>
+  handleClick(){
+    this.props.buySlot(this.props.web3, this.props.match.params.id);
   }
-  
-  var item = props.items[id]
-  return (
-    <div>
-      <h1>{item.seller} (#{item.id})</h1>
-      <h2>Slot Price: {item.slotPrice}</h2>
-    </div>
-  )
+
+  render(){
+    if(this.props.items === null){
+      return null
+    }
+
+    let id = parseInt(this.props.match.params.id);
+
+    if (isNaN(id)) {
+      return <div>Sorry, but no item</div>
+    }
+
+    var item = this.props.items[id]
+    return (
+      <div>
+        <div>
+          <h1>{item.seller} (#{item.id})</h1>
+          <h2>Slot Price: {item.slotPrice}</h2>
+        </div>
+        <Button onClick={this.handleClick}>
+          {'Buy slot'}
+        </Button>
+      </div>
+    )
+  }
 }
 
 DetailedItem.propTypes = {
-  item: PropTypes.object
+  items: PropTypes.object
 };
 
 function mapStateToProps(state) {
   return {
-    items: state.items
+    items: state.items,
+    web3: state.web3
   };
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     fetchItemDetails: bindActionCreators(fetchItemDetails, dispatch)
-//   };
-// }
+function mapDispatchToProps(dispatch) {
+  return {
+    buySlot: bindActionCreators(buySlot, dispatch)
+  };
+}
 
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(DetailedItem);
