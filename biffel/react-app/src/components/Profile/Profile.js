4ed37c0992dfd4483a5837d3b416cf77a457cbb3
mayala3 <mayala3@stanford.edu>
2018-06-05 00:39:14 -0700

    made reload possible in profile

--- biffel/react-app/src/components/Profile/Profile.js
+++ biffel/react-app/src/components/Profile/Profile.js
@@ -16,113 +16,114 @@ class Profile extends Component {
 
   render (){
 
-    var activeBiffelsUserSeller = getActiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount);
-    var activeBiffelsUserBuyer = getActiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount);
+    if(this.props.items && this.props.web3){
+      var activeBiffelsUserSeller = getActiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount);
+      var activeBiffelsUserBuyer = getActiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount);
 
-    var inactiveBiffelsUserSeller = getInactiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount);
-    var inactiveBiffelsUserBuyer = getInactiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount);
+      var inactiveBiffelsUserSeller = getInactiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount);
+      var inactiveBiffelsUserBuyer = getInactiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount);
 
-    // console.log('ActiveBiffelsUserSeller', getActiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount));
-    // console.log('ActiveBiffelsUserBuyer', getBiffelsUserBuyer(this.props.items, this.props.web3.userAccount));
+      // console.log('ActiveBiffelsUserSeller', getActiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount));
+      // console.log('ActiveBiffelsUserBuyer', getBiffelsUserBuyer(this.props.items, this.props.web3.userAccount));
 
-    // console.log('InactiveBiffelsUserSeller', getInactiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount));
-    // console.log('InactiveBiffelsUserBuyer', getInactiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount));
+      // console.log('InactiveBiffelsUserSeller', getInactiveBiffelsUserSeller(this.props.items, this.props.web3.userAccount));
+      // console.log('InactiveBiffelsUserBuyer', getInactiveBiffelsUserBuyer(this.props.items, this.props.web3.userAccount));
+      return (
+        <div>
+          <Panel>
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">Your Ethereum Address</Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>{this.props.web3.userAccount}</Panel.Body>
+          </Panel>
 
-    return (
-      <div>
-        <Panel>
-          <Panel.Heading>
-            <Panel.Title componentClass="h3">Your Ethereum Address</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body>{this.props.web3.userAccount}</Panel.Body>
-        </Panel>
+          <Tabs>
+            <Tab eventKey={1} title="Your Active Biffels" enabled>
+              <Panel>
+                <Panel.Body>
+                  {activeBiffelsUserSeller ?
+                    activeBiffelsUserSeller.map(i => {
+                      return(
+                        <Panel>
+                          <Panel.Heading>
+                            <Link to={`/buy/${i.id}`}>{i.title}</Link>
+                          </Panel.Heading>
+                        </Panel>
+                      )
+                    })
+                  :
+                    null
+                  }
+                </Panel.Body>
+              </Panel>
+            </Tab>
 
-        <Tabs>
-          <Tab eventKey={1} title="Your Active Biffels" enabled>
-            <Panel>
-              <Panel.Body>
-                {activeBiffelsUserSeller ?
-                  activeBiffelsUserSeller.map(i => {
-                    return(
-                      <Panel>
-                        <Panel.Heading>
-                          <Link to={`/buy/${i.id}`}>{i.title}</Link>
-                        </Panel.Heading>
-                      </Panel>
-                    )
-                  })
-                :
-                  null
-                }
-              </Panel.Body>
-            </Panel>
-          </Tab>
+            <Tab eventKey={2} title="Your Active Bids" enabled>
+              <Panel>
+                <Panel.Body>
+                  {activeBiffelsUserBuyer ?
+                    activeBiffelsUserBuyer.map(i => {
+                      return(
+                        <Panel>
+                          <Panel.Heading>
+                            <Link to={`/buy/${i.id}`}>{i.title}</Link>
+                          </Panel.Heading>
+                        </Panel>
+                      )
+                    })
+                  :
+                    null
+                  }
+                </Panel.Body>
+              </Panel>
+            </Tab>
 
-          <Tab eventKey={2} title="Your Active Bids" enabled>
-            <Panel>
-              <Panel.Body>
-                {activeBiffelsUserBuyer ?
-                  activeBiffelsUserBuyer.map(i => {
-                    return(
-                      <Panel>
-                        <Panel.Heading>
-                          <Link to={`/buy/${i.id}`}>{i.title}</Link>
-                        </Panel.Heading>
-                      </Panel>
-                    )
-                  })
-                :
-                  null
-                }
-              </Panel.Body>
-            </Panel>
-          </Tab>
+            <Tab eventKey={3} title="Your Past Biffels" enabled>
+              <Panel>
+                <Panel.Body>
+                  {inactiveBiffelsUserSeller ?
+                    inactiveBiffelsUserSeller.map(i => {
+                      return(
+                        <Panel>
+                          <Panel.Heading>
+                            <Link to={`/buy/${i.id}`}>{i.title}</Link>
+                          </Panel.Heading>
+                        </Panel>
+                      )
+                    })
+                  :
+                    null
+                  }
+                </Panel.Body>
+              </Panel>
+            </Tab>
 
-          <Tab eventKey={3} title="Your Past Biffels" enabled>
-            <Panel>
-              <Panel.Body>
-                {inactiveBiffelsUserSeller ?
-                  inactiveBiffelsUserSeller.map(i => {
-                    return(
-                      <Panel>
-                        <Panel.Heading>
-                          <Link to={`/buy/${i.id}`}>{i.title}</Link>
-                        </Panel.Heading>
-                      </Panel>
-                    )
-                  })
-                :
-                  null
-                }
-              </Panel.Body>
-            </Panel>
-          </Tab>
+            <Tab eventKey={4} title="Your Past Bids" enabled>
+              <Panel>
+                <Panel.Body>
+                  {inactiveBiffelsUserBuyer ?
+                    inactiveBiffelsUserBuyer.map(i => {
+                      return(
+                        <Panel>
+                          <Panel.Heading>
+                            <Link to={`/buy/${i.id}`}>{i.title}</Link>
+                          </Panel.Heading>
+                        </Panel>
+                      )
+                    })
+                  :
+                    null
+                  }
+                </Panel.Body>
+              </Panel>
+            </Tab>
 
-          <Tab eventKey={4} title="Your Past Bids" enabled>
-            <Panel>
-              <Panel.Body>
-                {inactiveBiffelsUserBuyer ?
-                  inactiveBiffelsUserBuyer.map(i => {
-                    return(
-                      <Panel>
-                        <Panel.Heading>
-                          <Link to={`/buy/${i.id}`}>{i.title}</Link>
-                        </Panel.Heading>
-                      </Panel>
-                    )
-                  })
-                :
-                  null
-                }
-              </Panel.Body>
-            </Panel>
-          </Tab>
+          </Tabs>
 
-        </Tabs>
-
-      </div>
-    )
-
+        </div>
+      )
+    }
+    return null
 
   }
 }
@@ -140,13 +141,13 @@ function mapStateToProps(state) {
 }
 
 function getActiveBiffelsUserSeller(items, userAccount) {
-  // var biffelsUserSeller = items.filter(item=> item.seller == userAccount)
-  var biffelsUserSeller = [];
-  for(var item in items){
-    if(items[item].seller == userAccount){
-      biffelsUserSeller.push(items[item]);
-    }
-  }
+  var biffelsUserSeller = items.filter(item=> item.seller == userAccount)
+  // var biffelsUserSeller = [];
+  // for(var item in items){
+  //   if(items[item].seller == userAccount){
+  //     biffelsUserSeller.push(items[item]);
+  //   }
+  // }
   return biffelsUserSeller;
 }
 
