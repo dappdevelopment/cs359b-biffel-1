d6e03dc576ed07573a355b2a75f5b38fa70095a0
Jaime Deverall <jaime1deverall@gmail.com>
2018-06-05 21:26:05 -0700

    dependencies

--- biffel/react-app/src/components/App/App.js
+++ biffel/react-app/src/components/App/App.js
@@ -38,7 +38,7 @@ class App extends Component{
       )
     }
     return (
-      <div class="MetamaskError">
+      <div className="MetamaskError">
         <img id="MetmaskErrorLogo" src="https://thumb.ibb.co/ctNaSo/34497858_1998022223846047_6463238276474994688_n.png"></img>
         <h3 id="MetamaskErrorTitle"> biffel </h3>
         <p> Please Log In To Metamask </p>
